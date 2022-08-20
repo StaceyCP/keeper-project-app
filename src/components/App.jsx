9050e97e72b4590b9e9f3eb31926238875536a68
Stacey <stacey6105@gmail.com>
2022-08-21 02:22:28 +0900

    imported stylesheet and created app components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="app">
+      <Header />
+      <Note />
+      <Footer />
+    </div>
   );
 }
 
